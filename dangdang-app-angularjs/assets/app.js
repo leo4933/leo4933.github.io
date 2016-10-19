bfcd1ae4eb49d1f104a038d046afdec26ffe05ec
leo4933 <657198623@qq.com>
2016-10-19 17:02:21 +0800

    跳转

--- dangdang-app-angularjs/assets/app.js
+++ dangdang-app-angularjs/assets/app.js
@@ -1,1 +1,21 @@
 var app = angular.module('app',['ngRoute']);
+
+app.config(['$routeProvider',function($routeProvider){
+  $routeProvider
+    .when('/',{
+      templateUrl:'./assets/tpl/index.html',
+      controller:''
+    })
+    .when('/my',{
+      templateUrl:'./assets/tpl/blog.html',
+      controller:''
+    })
+    .when('/hot',{
+      templateUrl:'./assets/tpl/book.html',
+      controller:'bookController'
+    })
+    .when('/link',{
+      templateUrl:'./assets/tpl/link.html',
+      controller:''
+    });
+}]);
--- dangdang-app-angularjs/assets/controllers/main_controller.js
+++ dangdang-app-angularjs/assets/controllers/main_controller.js
@@ -1,5 +1,11 @@
 app.controller('mainController',['$scope','commonService',function($scope,c_s){
-  c_s.getData('ertong',function(res){
-    console.dir(res);
-  });
+  // c_s.getData('ertong',function(res){
+  //   console.dir(res);
+  // });
+
+  $scope.selectedIndex = 0;
+  $scope.linkTo = function(index){
+    $scope.selectedIndex = index;
+    console.log(index);
+  };
 }]);
