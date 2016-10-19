3b6c5a4da24ac2dd4718956b5a121e123d48778c
leo4933 <657198623@qq.com>
2016-10-19 17:10:12 +0800

    fix

--- dangdang-app-angularjs/assets/app.js
+++ dangdang-app-angularjs/assets/app.js
@@ -3,7 +3,7 @@ var app = angular.module('app',['ngRoute']);
 app.config(['$routeProvider',function($routeProvider){
   $routeProvider
     .when('/',{
-      templateUrl:'./assets/tpl/index.html',
+      templateUrl:'index.html',
       controller:''
     })
     .when('/my',{
