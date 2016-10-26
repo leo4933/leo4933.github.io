a3a172933ae6bd4a60fa30012634bfda52f849dd
leo4933 <657198623@qq.com>
2016-10-26 17:24:57 +0800

    bug修改

--- dangdang-app-angularjs/assets/controllers/main_controller.js
+++ dangdang-app-angularjs/assets/controllers/main_controller.js
@@ -5,13 +5,14 @@ app.controller('mainController',['$scope',function($scope){
   var arr = window.location.href.split('/');
   var lastStr = arr[arr.length-1];
   console.log(lastStr);
+
   switch (lastStr) {
     case 'hot':$scope.selectedIndex = 2;
-      return ;
+      break ;
     default: $scope.selectedIndex = 0;
-      return;
+      break;
   }
-    
+    // $scope.selectedIndex = 0;
     $scope.linkTo = function(index){
     $scope.selectedIndex = index;
     console.log(index);
